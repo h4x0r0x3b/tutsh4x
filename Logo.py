dfb8b1f58de768456efda811f37e1a82ef2176af
h4x0r0x3b <h4x0r0x3b@github.com>
2022-12-31 10:11:06 +0800

    educational purposes for ethical hacking

---
+++ Logo.py
@@ -0,0 +1,18 @@
+RED='\033[31m'
+BLUE='\033[34m'
+WHITE='\033[37m'
+YELLOW='\033[33m'
+msg = f"""
+{WHITE} ████{YELLOW}██{WHITE}╗██{BLUE}╗   ██╗██████╗ ███████╗██████╗ P
+{WHITE}██╔════╝╚██╗ {BLUE}██╔╝██╔══██╗██╔════╝██╔══██╗H
+{WHITE}██║      ╚████╔╝ {BLUE}██████╔╝█████╗  ██████╔╝I
+{WHITE}██║       ╚{YELLOW}██{WHITE}╔╝  ██╔══{BLUE}██╗██╔══╝  ██╔══██╗L
+{WHITE}╚████{YELLOW}██╗   ██║   █{WHITE}█████╔╝{BLUE}███████╗██║  ██║I
+{WHITE} ╚════{YELLOW}═╝   ╚═╝   ╚═{WHITE}═══{YELLOW}═╝ ╚{WHITE}══{BLUE}════╝╚═╝  ╚═╝P
+{WHITE} ███╗   {YELLOW}██╗██╗███{WHITE}╗   █{YELLOW}█╗{WHITE}     {RED}██╗ █████╗  P
+{WHITE} ████╗  {YELLOW}██║██║██{WHITE}██╗  ██{RED}║     ██║██╔══██╗ I
+{WHITE} ██╔██╗ ██║{YELLOW}██║{WHITE}██╔██╗ {RED}██║     ██║███████║ N
+{WHITE} ██║╚██╗██║██║██║{RED}╚██╗██║██   ██║██╔══██║ E
+{WHITE} ██║ ╚{YELLOW}██{WHITE}██║██║{RED}██║ ╚████║╚█████╔╝██║  ██║ S
+{WHITE} ╚═╝  ╚═══╝{RED}╚═╝╚═╝  ╚═══╝ ╚════╝ ╚═╝  ╚═╝ ™"""
+print(msg)
